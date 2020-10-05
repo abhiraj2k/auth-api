869dafa1309ebccbaf838d5d9031fc51aa821b3b
Abhishek Bhavsar <abhiraj2k@gmail.com>
2020-10-05 21:34:00 +0530

    Updates

---
+++ precache-manifest.9d987a2537a4fb3771aa9ba77a0511c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "151abf75b649cb7fb37d9bbc83d66293",
+    "url": "/abhiraj2k/auth-api.git/index.html"
+  },
+  {
+    "revision": "1e96ee1ef4885f440459",
+    "url": "/abhiraj2k/auth-api.git/static/css/main.e0d6757d.chunk.css"
+  },
+  {
+    "revision": "35bbea379dbe4b077c1b",
+    "url": "/abhiraj2k/auth-api.git/static/js/2.c108b0fa.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/abhiraj2k/auth-api.git/static/js/2.c108b0fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e96ee1ef4885f440459",
+    "url": "/abhiraj2k/auth-api.git/static/js/main.bc8c0531.chunk.js"
+  },
+  {
+    "revision": "10b15b07aca455dfa03e",
+    "url": "/abhiraj2k/auth-api.git/static/js/runtime-main.ddd7d2ce.js"
+  }
+]);
